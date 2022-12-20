30bf44b629213643c8244da32f32060dc41f8968
Daniel Kim <danielkim@daniels-mbp.lan>
2022-12-20 10:16:20 -0800

    minor

--- ngin.py
+++ ngin.py
@@ -50,52 +50,53 @@ class ServiceListener:
     pass
 
 class EventHandler:
+  unexpected = 'Unexpected:'
   def handle(self, c):
     match c.head:
       case Head.key:
-        self.keyHandler(c)
+        self.key_handler(c)
       case Head.contact:
-        self.contactHandler(c)
+        self.contact_handler(c)
       case Head.event:
-        self.eventHandler(c)
+        self.event_handler(c)
       case Head.directional:
-        self.directionalHandler(c)
+        self.directional_handler(c)
       case Head.button:
-        self.buttonHandler(c)
+        self.button_handler(c)
       case _:
-        print('Unexpected:', c)
+        print(self.unexpected, c)
 
-  def keyHandler(self, c):
-    print('Unexpected:', c)     
-  def contactHandler(self, c):
-    print('Unexpected:', c) 
-  def eventHandler(self, c):  
-    print('Unexpected:', c)   
-  def directionalHandler(self, c):    
-    print('Unexpected:', c) 
-  def buttonHandler(self, c):
-    print('Unexpected:', c) 
+  def key_handler(self, c):
+    print(self.unexpected, c)     
+  def contact_handler(self, c):
+    print(self.unexpected, c) 
+  def event_handler(self, c):  
+    print(self.unexpected, c)   
+  def directional_handler(self, c):    
+    print(self.unexpected, c) 
+  def button_handler(self, c):
+    print(self.unexpected, c) 
 
 class Recv:
   def __init__(self, socket:socket) -> None:
     self.remaining = 0
     self.socket = socket
-    self.returnAck = False
-    self.returnCmd = False
+    self.return_ack = False
+    self.return_cmd = False
 
-  def waitAck(self):
-    self.returnAck = True
-    r = self.eventLoop()
-    self.returnAck = False
+  def wait_ack(self):
+    self.return_ack = True
+    r = self.event_loop()
+    self.return_ack = False
     return r
 
-  def waitCmd(self):
-    self.returnCmd = True
-    r = self.eventLoop()
-    self.returnCmd = False
+  def wait_cmd(self):
+    self.return_cmd = True
+    r = self.event_loop()
+    self.return_cmd = False
     return r   
 
-  def eventLoop(self):
+  def event_loop(self):
     while True:    
       if self.remaining < 4:
         if self.remaining == 0:
@@ -123,14 +124,14 @@ class Recv:
       match c.head:
         case Head.ack:
           c = c.ack
-          if self.returnAck:
+          if self.return_ack:
             #print(f'ACK:{c.code} {c.info}')          
             return c.code
           else:
             print(f'Unexpected: ACK - {c.code} {c.info}')
         case Head.cmd:
           c = c.cmd
-          if self.returnCmd:
+          if self.return_cmd:
             #print(f'Cmd:{c}')          
             return c
           else:
@@ -156,7 +157,7 @@ class Nx:
     self.socket.connect((HOST, self.port))
     self.recv = Recv(self.socket)
 
-  def setEventHandler(self, handler):
+  def set_event_handler(self, handler):
     self.recv.handler = handler
 
   def find_ip(self, type, verbose=False):
@@ -180,9 +181,9 @@ class Nx:
     #print(f'size:4 + {len(bs)}')
     self.socket.sendall(head_bytes + len_bytes + bs)
     if(ack):
-      return self.recv.waitAck()
+      return self.recv.wait_ack()
 
-  def stageBuilder(self, width:float, height:float):
+  def stage_builder(self, width:float, height:float):
     c = CStageInfo()
     c.background = 'Blue'
     c.gravityX = 0
@@ -196,7 +197,7 @@ class Nx:
     c.button2 = ActionEvent.DOWN
     return c
 
-  def tilesBuilder(self, path:str, tileSize:float, width:float, height:float, data:list[int]):
+  def tiles_builder(self, path:str, tile_size:float, width:float, height:float, data:list[int]):
     c = CObject()
     c.tid = 0
     v = c.visible       
@@ -213,22 +214,22 @@ class Nx:
     a = CAction()
     a.path = path
     a.stepTime = 200/1000
-    a.tileSizeX = tileSize
-    a.tileSizeY = tileSize
+    a.tileSizeX = tile_size
+    a.tileSizeY = tile_size
     a.indices.extend(data)
     a.repeat = False
     a.type = CActionType.tiles
     v.actions.append(a)
     return c
 
-  def objBuilder(self, id:int, info:str) -> CObject:
+  def obj_builder(self, id:int, info:str) -> CObject:
     c = CObject()
     c.tid = 0
     c.id = id
     c.info = info
     return c
 
-  def physicalBuilder(self, obj:CObject, shape:BodyShape, x:float, y:float) -> CPhysical:
+  def physical_builder(self, obj:CObject, shape:BodyShape, x:float, y:float) -> CPhysical:
     p = obj.physical
     p.x = x
     p.y = y
@@ -249,7 +250,7 @@ class Nx:
     p.shape = shape
     return p
 
-  def visibleBuilder(self, obj:CObject, actions:list[CAction]):
+  def visible_builder(self, obj:CObject, actions:list[CAction]):
     v = obj.visible
     v.current = CActionType.idle
     v.priority = 0
@@ -264,19 +265,19 @@ class Nx:
     v.actions.extend(actions)
     return v
 
-  def actionBuilder(self, path:str, tileSize:float, indices:list[int], type:CActionType = CActionType.idle, repeat:bool=True) -> CAction:
+  def action_builder(self, path:str, tile_size:float, indices:list[int], type:CActionType = CActionType.idle, repeat:bool=True) -> CAction:
     a = CAction()
     a.path = path
-    a.tileSizeX = tileSize
-    a.tileSizeY = tileSize
+    a.tileSizeX = tile_size
+    a.tileSizeY = tile_size
     a.indices.extend(indices)
     a.stepTime = 0.2
     a.type = type
     a.repeat = repeat
     return a
 
-  def mainLoop(self):
-    self.recv.eventLoop()
+  def main_loop(self):
+    self.recv.event_loop()
 
 
   def follow(self, id:int) -> None:
@@ -304,23 +305,23 @@ class Nx:
     c.ints.append(4041)
     self.send(Head.cmd, c)
 
-  def getObjInfo(self, id:int):
+  def get_obj_info(self, id:int):
     c = Cmd()
     c.strings.append('objinfo')
     c.ints.append(id)
     self.send(Head.cmd, c)
-    v = self.recv.waitCmd()
+    v = self.recv.wait_cmd()
     return v
 
-  def setActionType(self, actionType:CActionType, isFlipHorizontal:bool = False) -> None:
+  def set_action_type(self, action_type:CActionType, is_flip_horizontal:bool = False) -> None:
     c = Cmd()
     c.strings.append('actionType')
     c.ints.append(id)
-    c.ints.append(1 if isFlipHorizontal == True else 0)    
-    c.ints.append(actionType)
+    c.ints.append(1 if is_flip_horizontal == True else 0)    
+    c.ints.append(action_type)
     self.send(Head.cmd, c)
 
-  def linearTo(self, id:int, x:float, y:float, speed:float):
+  def linear_to(self, id:int, x:float, y:float, speed:float):
     c = Cmd()
     c.strings.append('linearTo')
     c.ints.append(id)
@@ -412,132 +413,3 @@ class Nx:
     c.ints.append(id)
     c.floats.append(time)
     self.send(Head.cmd, c)    
- 
-
-
-class MyHandler(EventHandler):
-  def __init__(self, nx):
-    self.nx = nx
-    self.nx = nx;        
-    self.key_down_left = False
-    self.key_down_right = False
-    self.actor_contacts = set()
-    self.actor_jump_count = 0
-    self.dynamic_id = 1000
-    self.facingLeft = False  
-    self.ready = False          
-
-  def goRight(self):
-    self.nx.angular(100, 1)
-
-  def goLeft(self):
-    self.nx.angular(100, -1)
-
-  def stop(self):
-    self.nx.angular(100, 0)
-
-
-  def missile(self):
-    info = self.nx.getObjInfo(100)
-    info = CObjectInfo(info.floats)
-
-    o = self.nx.objBuilder(101, "missile")
-    p = self.nx.physicalBuilder(o, BodyShape.rectangle, info.x-0.5 + 2*math.sin(info.angle), info.y-0.5 - 2*math.cos(info.angle))
-    p.angle = info.angle
-    v = self.nx.visibleBuilder(o, [nx.actionBuilder('kenney_pixelshmup/tiles_packed.png', 16, [1, 2, 3])])
-    self.nx.send(Head.cobject, o, True)
-    self.nx.forward(101, 0, 20)
-    self.nx.timer(101, 1)
-
-  def keyHandler(self, c):
-    c = c.key
-    if c.isPressed == False:
-      match c.name:
-        case 'Arrow Left':
-          self.key_down_left = False
-          if self.key_down_right:
-            self.goRight()
-          else:
-            self.stop()	
-        case 'Arrow Right':
-          self.key_down_right = False
-          if self.key_down_left:
-            self.goLeft()
-          else:
-            self.stop()
-    else:
-      match c.name:
-        case 'Arrow Left':
-          self.key_down_left = True         
-          self.goLeft()
-        case 'Arrow Right':
-          self.key_down_right = True            
-          self.goRight()
-        case 'Arrow Up':	
-          self.missile()
-
-  def contactHandler(self, c):
-    contact = c.contact
-    if contact.isEnded == False:
-      if contact.id1 == 101:
-        self.nx.remove(contact.id1)
-
-        o = self.nx.objBuilder(1000, "fire")
-        o.tid = contact.id2
-        v = self.nx.visibleBuilder(o, [nx.actionBuilder('kenney_pixelshmup/tiles_packed.png', 16, [5])])
-        self.nx.send(Head.cobject, o, True)
-
-  def eventHandler(self, c):
-    print(c)
-
-
-if __name__ == "__main__":
-  nx = Nx('bonsoirdemo', 4040)
-  nx.setEventHandler(MyHandler(nx))
-  f = open('./data/planes0.tmj', "r")
-  j = json.loads(f.read())
-  f.close()
-  t = j['layers'][0]
-  tileSize = j['tilewidth']
-  nx.send(Head.stage, nx.stageBuilder(t['width'], t['height']), True)  
-
-  nx.send(Head.cobject, nx.tilesBuilder('kenney_pixelshmup/tiles_packed.png', tileSize, t['width'], t['height'], t['data']))
-
-  o = nx.objBuilder(100, "hero")
-  p = nx.physicalBuilder(o, BodyShape.circle, 11, 11)
-  p.angle = 1.5
-  p.width = 2
-  p.height = 2
-  v = nx.visibleBuilder(o, [nx.actionBuilder('kenney_pixelshmup/ships_packed.png', 32, [1])])
-  v.width = 2
-  v.height = 2
-  nx.send(Head.cobject, o, True)
-
-  nx.follow(100)
-  nx.forward(100, 0, 5)
-
-  o = nx.objBuilder(200, "enemy")
-  p = nx.physicalBuilder(o, BodyShape.circle, 11, 0)
-  p.width = 2
-  p.height = 2
-  v = nx.visibleBuilder(o, [nx.actionBuilder('kenney_pixelshmup/ships_packed.png', 32, [10])])
-  v.width = 2
-  v.height = 2
-  nx.send(Head.cobject, o, True)
-
-  nx.forward(200, 0, 5)
-  nx.angular(200, 1)
-
-  nx.mainLoop()
-
-
-
-
-
-
-
-
-
-
-
-
--- plane.py
+++ plane.py
@@ -16,93 +16,93 @@ class MyHandler(EventHandler):
     self.facingLeft = False  
     self.ready = False          
 
-  def goRight(self):
+  def go_right(self):
     self.nx.angular(100, 1)
 
-  def goLeft(self):
+  def go_left(self):
     self.nx.angular(100, -1)
 
   def stop(self):
     self.nx.angular(100, 0)
 
-  def getDynamicId(self):
+  def get_dynamic_id(self):
     self.dynamic_id += 1
     return self.dynamic_id
 
   def missile(self):
-    info = self.nx.getObjInfo(100)
+    info = self.nx.get_obj_info(100)
     info = CObjectInfo(info.floats)
-    new_id = self.getDynamicId()
-    o = self.nx.objBuilder(new_id, "missile")
+    new_id = self.get_dynamic_id()
+    o = self.nx.obj_builder(new_id, "missile")
 
-    p = self.nx.physicalBuilder(o, BodyShape.rectangle, info.x-0.5 + 2*math.sin(info.angle), info.y-0.5 - 2*math.cos(info.angle))
+    p = self.nx.physical_builder(o, BodyShape.rectangle, info.x-0.5 + 2*math.sin(info.angle), info.y-0.5 - 2*math.cos(info.angle))
     p.angle = info.angle
-    v = self.nx.visibleBuilder(o, [nx.actionBuilder('kenney_pixelshmup/tiles_packed.png', 16, [1, 2, 3])])
+    v = self.nx.visible_builder(o, [nx.action_builder('kenney_pixelshmup/tiles_packed.png', 16, [1, 2, 3])])
     self.nx.send(Head.cobject, o, True)
     self.nx.forward(new_id, 0, 20)
     self.nx.timer(new_id, 0.7)
 
-  def keyHandler(self, c):
+  def key_handler(self, c):
     c = c.key
     if c.isPressed == False:
       match c.name:
         case 'Arrow Left':
           self.key_down_left = False
           if self.key_down_right:
-            self.goRight()
+            self.go_right()
           else:
             self.stop()	
         case 'Arrow Right':
           self.key_down_right = False
           if self.key_down_left:
-            self.goLeft()
+            self.go_left()
           else:
             self.stop()
     else:
       match c.name:
         case 'Arrow Left':
           self.key_down_left = True         
-          self.goLeft()
+          self.go_left()
         case 'Arrow Right':
           self.key_down_right = True            
-          self.goRight()
+          self.go_right()
         case 'Arrow Up':	
           self.missile()
 
-  def contactHandler(self, c):
+  def contact_handler(self, c):
     contact = c.contact
     if contact.isEnded == False:
       if contact.info2 == 'missile':
         self.nx.remove(contact.id2)
 
-        o = self.nx.objBuilder(self.getDynamicId(), "fire")
+        o = self.nx.obj_builder(self.get_dynamic_id(), "fire")
         o.tid = contact.id1
-        v = self.nx.visibleBuilder(o, [nx.actionBuilder('kenney_pixelshmup/tiles_packed.png', 16, [5])])
+        self.nx.visible_builder(o, [nx.action_builder('kenney_pixelshmup/tiles_packed.png', 16, [5])])
         self.nx.send(Head.cobject, o, True)
 
-  def eventHandler(self, c):
+  def event_handler(self, c):
     event = c.event
     if event.info == 'missile':
       self.nx.remove(event.id)
 
 if __name__ == "__main__":
   nx = Nx('bonsoirdemo', 4040)
-  nx.setEventHandler(MyHandler(nx))
+  nx.set_event_handler(MyHandler(nx))
   f = open('./data/planes0.tmj', "r")
   j = json.loads(f.read())
   f.close()
   t = j['layers'][0]
   tileSize = j['tilewidth']
-  nx.send(Head.stage, nx.stageBuilder(t['width'], t['height']), True)  
+  nx.send(Head.stage, nx.stage_builder(t['width'], t['height']), True)  
 
-  nx.send(Head.cobject, nx.tilesBuilder('kenney_pixelshmup/tiles_packed.png', tileSize, t['width'], t['height'], t['data']))
+  nx.send(Head.cobject, nx.tiles_builder('kenney_pixelshmup/tiles_packed.png', tileSize, t['width'], t['height'], t['data']))
 
-  o = nx.objBuilder(100, "hero")
-  p = nx.physicalBuilder(o, BodyShape.circle, 11, 11)
+  o = nx.obj_builder(100, "hero")
+  p = nx.physical_builder(o, BodyShape.circle, 11, 11)
   p.angle = 1.5
   p.width = 2
   p.height = 2
-  v = nx.visibleBuilder(o, [nx.actionBuilder('kenney_pixelshmup/ships_packed.png', 32, [1])])
+  v = nx.visible_builder(o, [nx.action_builder('kenney_pixelshmup/ships_packed.png', 32, [1])])
   v.width = 2
   v.height = 2
   nx.send(Head.cobject, o, True)
@@ -110,11 +110,11 @@ if __name__ == "__main__":
   nx.follow(100)
   nx.forward(100, 0, 5)
 
-  o = nx.objBuilder(200, "enemy")
-  p = nx.physicalBuilder(o, BodyShape.circle, 11, 0)
+  o = nx.obj_builder(200, "enemy")
+  p = nx.physical_builder(o, BodyShape.circle, 11, 0)
   p.width = 2
   p.height = 2
-  v = nx.visibleBuilder(o, [nx.actionBuilder('kenney_pixelshmup/ships_packed.png', 32, [10])])
+  v = nx.visible_builder(o, [nx.action_builder('kenney_pixelshmup/ships_packed.png', 32, [10])])
   v.width = 2
   v.height = 2
   nx.send(Head.cobject, o, True)
@@ -122,4 +122,4 @@ if __name__ == "__main__":
   nx.forward(200, 0, 5)
   nx.angular(200, 1)
 
-  nx.mainLoop()
+  nx.main_loop()
