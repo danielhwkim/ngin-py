06a9ca49f262441fd1d6e22672b6476bb46dd3bf
Daniel Kim <danielkim@daniels-mbp.lan>
2022-12-27 13:41:46 -0800

    plane2

---
+++ plane2.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+from command_pb2 import Head, CStageInfo, JoystickDirectionals, ActionEvent, CmdInfo, CObject, CVisible, CPhysical, CAction, CActionType, BodyShape, BodyType, Cmd
+import json
+import math
+from ngin import Nx, EventHandler, CObjectInfo
+
+class MyHandler(EventHandler):
+  def __init__(self, nx:Nx):
+    self.nx = nx      
+    self.key_down_left = False
+    self.key_down_right = False
+    self.key_down_left2 = False
+    self.key_down_right2 = False    
+    self.dynamic_id = 1000      
+
+  def go_right(self, id):
+    self.nx.angular(id, 1)
+
+  def go_left(self, id):
+    self.nx.angular(id, -1)
+
+  def stop(self, id):
+    self.nx.angular(id, 0)
+
+  def get_dynamic_id(self):
+    self.dynamic_id += 1
+    return self.dynamic_id
+
+  def missile(self, id):
+    info = self.nx.get_obj_info(id)
+    info = CObjectInfo(info.floats)
+    new_id = self.get_dynamic_id()
+    o = self.nx.obj_builder(new_id, "missile")
+
+    p = self.nx.physical_builder(o, BodyShape.rectangle, info.x-0.5 + 2*math.sin(info.angle), info.y-0.5 - 2*math.cos(info.angle))
+    p.angle = info.angle
+    v = self.nx.visible_builder(o, [nx.action_builder('kenney_pixelshmup/tiles_packed.png', 16, [1, 2, 3])])
+    self.nx.send(Head.cobject, o, True)
+    self.nx.forward(new_id, 0, 20)
+    self.nx.timer(new_id, 0.7)
+    self.nx.audio_play('sfx/fire_1.mp3')
+
+  def key_handler(self, c):
+    print(c)
+    c = c.key
+    if c.isPressed == False:
+      if c.name == 'Arrow Left':
+        self.key_down_left = False
+        if self.key_down_right:
+          self.go_right(100)
+        else:
+          self.stop(100)	
+      elif c.name == 'Arrow Right':
+        self.key_down_right = False
+        if self.key_down_left:
+          self.go_left(100)
+        else:
+          self.stop(100)
+      elif c.name == 'A':
+        self.key_down_left2 = False
+        if self.key_down_right2:
+          self.go_right(200)
+        else:
+          self.stop(200)	
+      elif c.name == 'D':
+        self.key_down_right2 = False
+        if self.key_down_left2:
+          self.go_left(200)
+        else:
+          self.stop(200)          
+    else:
+      if c.name == 'Arrow Left':
+        self.key_down_left = True         
+        self.go_left(100)
+      elif c.name == 'Arrow Right':
+        self.key_down_right = True            
+        self.go_right(100)
+      elif c.name == 'Arrow Up':	
+        self.missile(100)
+      elif c.name == 'A':
+        self.key_down_left2 = True         
+        self.go_left(200)
+      elif c.name == 'D':
+        self.key_down_right2 = True            
+        self.go_right(200)
+      elif c.name == 'W':	
+        self.missile(200)        
+
+  def contact_handler(self, c):
+    contact = c.contact
+    if contact.isEnded == False and contact.info2 == 'missile':
+      self.nx.remove(contact.id2)
+
+      o = self.nx.obj_builder(self.get_dynamic_id(), "fire")
+      o.tid = contact.id1
+      self.nx.visible_builder(o, [nx.action_builder('kenney_pixelshmup/tiles_packed.png', 16, [5])])
+      self.nx.send(Head.cobject, o, True)
+
+  def event_handler(self, c):
+    event = c.event
+    if event.info == 'missile':
+      self.nx.remove(event.id)
+
+if __name__ == "__main__":
+  nx = Nx('bonsoirdemo', 4040)
+  nx.set_event_handler(MyHandler(nx))
+  nx.bgm_play('music/bg_music.mp3')
+  f = open('./data/planes0.tmj', "r")
+  j = json.loads(f.read())
+  f.close()
+  t = j['layers'][0]
+  tileSize = j['tilewidth']
+  stage = nx.stage_builder(70, 40)
+  stage.debug = True
+  nx.send(Head.stage, stage, True)  
+
+  nx.send(Head.cobject, nx.tiles_builder('kenney_pixelshmup/tiles_packed.png', tileSize, t['width'], t['height'], t['data']))
+
+  o = nx.obj_builder(10000, "wall")
+  p = nx.physical_builder(o, BodyShape.rectangle, 0, 0)
+  p.type = BodyType.static
+  p.width = 70
+  p.height = 1  
+  nx.send(Head.cobject, o, True)
+  p.width = 1
+  p.height = 40  
+  nx.send(Head.cobject, o, True)
+  p.x=70-1
+  nx.send(Head.cobject, o, True)  
+  p.x=0
+  p.y=40-1
+  p.width = 70
+  p.height = 1
+  nx.send(Head.cobject, o, True) 
+
+
+  o = nx.obj_builder(100, "hero")
+  p = nx.physical_builder(o, BodyShape.circle, 60, 30)
+  #p.angle = 1.5
+  p.width = 2
+  p.height = 2
+  v = nx.visible_builder(o, [nx.action_builder('kenney_pixelshmup/ships_packed.png', 32, [1])])
+  v.width = 2
+  v.height = 2
+  nx.send(Head.cobject, o, True)
+
+  #nx.follow(100)
+  nx.forward(100, 0, 5)
+
+  o = nx.obj_builder(200, "enemy")
+  p = nx.physical_builder(o, BodyShape.circle, 10, 10)
+  p.angle = 3.14
+  p.width = 2
+  p.height = 2
+  v = nx.visible_builder(o, [nx.action_builder('kenney_pixelshmup/ships_packed.png', 32, [10])])
+  v.width = 2
+  v.height = 2
+  nx.send(Head.cobject, o, True)
+
+  nx.forward(200, 0, 5)
+  #nx.angular(200, 1)
+  #nx.follow(200)
+
+  nx.main_loop()
